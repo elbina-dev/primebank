7618e076084c93326ceaf0b1f833b4cc72e05004
favanna21 <favanna21@gmail.com>
2026-09-23 11:43:45 +0100

    Add generate_random_num seed-based account number helper

--- app.py
+++ app.py
@@ -51,6 +51,22 @@ ALLOWED_FORMATS = ["jpg", "jpeg", "png"]
 MAX_UPLOAD_BYTES = 2 * 1024 * 1024  # 2 MB
 
 
+def generate_random_num():
+    """function to generate a list - 5 num - of further adjustable nunber from a prelist see from os"""
+    seed_list = os.getenv("SEED_LIST", "1,2,3,4,5,6,7,8,9").split(",")
+    num_list=[random.choice(seed_list) for _ in range(7)]
+    num_list = [num_list[i] for i in range(5)]
+    num_list_int=[]
+    for i in num_list:
+        """use the seed to create random values"""
+        i = int(i)
+        num = str(random.randint(0, i))
+        num_list_int.append(i)
+    return num_list_int
+        
+
+
+
 def _read_initial_balance():
     """Read the starting balance from the environment.
 
